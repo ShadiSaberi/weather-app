d700261a985b048f7596da7c5f0030f61a068278
ShadiSaberi <shadi.sn93@gmail.com>
2025-03-23 16:47:17 +0330

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 function refreshWeather(response) {
-  let temperatureElement = document.querySelector("#temerature");
+  let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-  let cityElement =
-    document.querySelector(
-      "#city"
-    ); /*المنت سیتی برابر باشه با مقادری که برای ای دی ستی میدیم*/
-  cityElement.innerHTML =
-    response.data.city; /*  - و innerHTML برای خواندن یا نوشتن محتوای HTML یک عنصر استفاده می‌شود.
-ورودی که از کاربر گرفتیم توی المنت سرچ اینپوت بیاد بشینه توی این*/
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  console.log(response.data);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -26,12 +51,6 @@ function handelSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-let searchFormElement =
-  document.querySelector(
-    "#search-form-id"
-  ); /*انتخاب یک عنصر با آی دی مشخص شده (که در اینجا بخش فرم هست) و متغیر سرچ فور المنت رو توی اون عنصر ذخیره میکنه*/
-searchFormElement.addEventListener(
-  "submit",
-  handelSearchSubmit
-); /*المنت سرچ فرم اینپونت بیاد هر وقت سابمیتش بکنیم، تابع هندل سرچ فلان رو برامون بخونه*/
+let searchFormElement = document.querySelector("#search-form-id");
+searchFormElement.addEventListener("submit", handelSearchSubmit);
 searchCity("Mashhad");
