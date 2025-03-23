5956f38d782e364b724aa230563fd687dda5436b
ShadiSaberi <shadi.sn93@gmail.com>
2025-03-23 15:17:01 +0330

    Added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-function handelSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector(
-    "#search-form-input-id"
-  ); /* دریافت مقدار ورودی کاربر*/
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temerature");
+  let temperature = response.data.temperature.current;
+
   let cityElement =
     document.querySelector(
       "#city"
     ); /*المنت سیتی برابر باشه با مقادری که برای ای دی ستی میدیم*/
   cityElement.innerHTML =
-    searchInput.value; /*  - و innerHTML برای خواندن یا نوشتن محتوای HTML یک عنصر استفاده می‌شود.
+    response.data.city; /*  - و innerHTML برای خواندن یا نوشتن محتوای HTML یک عنصر استفاده می‌شود.
 ورودی که از کاربر گرفتیم توی المنت سرچ اینپوت بیاد بشینه توی این*/
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "f05a484ed430ce4d21tad607a48ebo0f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handelSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(
+    "#search-form-input-id"
+  ); /* دریافت مقدار ورودی کاربر*/
+  searchCity(searchInput.value);
 }
 
 let searchFormElement =
@@ -20,3 +34,4 @@ searchFormElement.addEventListener(
   "submit",
   handelSearchSubmit
 ); /*المنت سرچ فرم اینپونت بیاد هر وقت سابمیتش بکنیم، تابع هندل سرچ فلان رو برامون بخونه*/
+searchCity("Mashhad");
